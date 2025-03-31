33e431cced18c09b2e8b6772a24115ddf91f4746
racfretguy <rachitagarwal80@gmail.com>
2025-03-31 22:18:47 +0530

    started implementing debouncing

--- code.js
+++ code.js
@@ -1,29 +1,51 @@
 const apiKey="f4db925a3d6842e5a0775307252703";
-let query="Jaipur"; 
+const url="http://api.weatherapi.com/v1/current.json?key=";
+let query; 
 
+search();
 
-fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`)
-.then((response)=>{
-    return response.json();
-})
-.then((data)=>{
-    console.log(data);
-    console.log(data.current.temp_c);
-    populateData(data); 
-})
-.catch((error)=>{
-    console.log(error);
-})
-
-
+function getData(query){
+   
+    fetch(`${url}${apiKey}&q=${query}`)
+        .then((response)=>{
+            console.log(response);
+            
+            return response.json();
+        })
+        .then((data)=>{
+            console.log(data);            
+            console.log(data.current.temp_c);
+            populateData(data); 
+        })
+        .catch((error)=>{
+            let errorHtml=document.getElementById("list");
+            errorHtml.appendChild(document.createElement("li"));
+            console.log(error.message);
+            
+        })
+    
+}
 function suggestions(){
 
 }
 
 function search(){
+
+
+    let searchPlace=document.getElementById("searchBar");
+    searchPlace.addEventListener("search",()=>{
+        query=searchPlace.value;
+        getData(query);
+        searchPlace.value="";
+    })
     
 }
 
 function populateData(data){
-    
+
+    let listOfData = document.getElementById("list");
+    listOfData.appendChild(document.createElement("li"));
+    listOfData.textContent=data.current.temp_c+" "+data.current.condition.text;
+
+
 }
